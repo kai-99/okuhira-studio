57cd618db59eaf4de78e7e483c3e51201c48a7a3
kai <k18future@gmail.com>
2021-06-21 22:06:49 +0900

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
 			resolve: "gatsby-plugin-offline",
 			options: {
 				workboxConfig: {
-					globPatterns: ["**/icon-path*"],
+					globPatterns: ["**/src/images/icon-path*"],
 				},
 			},
 		},
