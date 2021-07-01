a3e36f65b7195979fc0bd5b6b3ab82e2cd5e0f88
kai <k18future@gmail.com>
2021-07-01 23:59:16 +0900

    sitemap fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
 			resolve: "gatsby-plugin-robots-txt",
 			options: {
 				host: "https://okuhira.studio",
-				sitemap: "https://okuhira.studio/sitemap.xml",
+				sitemap: "https://okuhira.studio/sitemap/sitemap-0.xml",
 				policy: [{ userAgent: "*", allow: "/" }],
 			},
 		},
