f4771a11e7aa3b3fcc12c50e338b2783dd82966b
kai <k18future@gmail.com>
2021-07-16 21:54:15 +0900

    Adsense fix

--- src/components/GoogleAds.jsx
+++ src/components/GoogleAds.jsx
@@ -4,12 +4,11 @@ import AdSense from "react-adsense";
 const GoogleAds = () => (
 	<>
 		<AdSense.Google
-			className="adsbygoogle"
 			style={{ display: "block" }}
 			client="ca-pub-4800021914562133"
 			slot="3370650941"
 			format="auto"
-			responsive={true}
+			responsive="true"
 		/>
 	</>
 );
