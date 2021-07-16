ffb284d6be036e657c22c9e460bd37499d4a963f
kai <k18future@gmail.com>
2021-07-16 22:09:07 +0900

    adfix

--- src/components/GoogleAds.jsx
+++ src/components/GoogleAds.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AdSense from "react-adsense";
 
-const GoogleAds = () => (
-	<>
-		<AdSense.Google
-			style={{ display: "block" }}
-			client="ca-pub-4800021914562133"
-			slot="3370650941"
-			format="auto"
-			responsive="true"
-		/>
-	</>
-);
+const GoogleAds = () => {
+	useEffect(() => {
+		if (window) {
+			window.adsbygoogle = window.adsbygoogle || [];
+			window.adsbygoogle.push({});
+		}
+	});
+	return (
+		<>
+			<AdSense.Google
+				style={{ display: "block" }}
+				client="ca-pub-4800021914562133"
+				slot="3370650941"
+				format="auto"
+				responsive="true"
+			/>
+		</>
+	);
+};
 export default GoogleAds;
